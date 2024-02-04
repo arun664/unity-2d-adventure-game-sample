a6f20f6ac88b08016d45f2c92acbd2b97d677805
Arun Kumar M <marunkumar664@gmail.com>
2024-02-04 23:16:34 +0530

    handle speed of player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
         MoveAction.Enable();
     }
 
@@ -18,7 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2) transform.position + move * 0.01f;
+        Vector2 position = (Vector2) transform.position + move * 3.0f * Time.deltaTime;
         transform.position = position;
     }
 }
