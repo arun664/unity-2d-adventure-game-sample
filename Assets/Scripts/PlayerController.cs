4e3035bc4464f919cb9b0c974099733a1e0bc03a
Arun Kumar M <marunkumar664@gmail.com>
2024-02-04 22:46:34 +0530

    optimized input system for player movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,46 +5,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public InputAction leftAction;
+    public InputAction MoveAction;
 
     // Start is called before the first frame update
     void Start()
     {
-        leftAction.Enable();
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.01f;
-        float vertical = 0.01f;
-
-        if (leftAction.IsPressed())
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-
-            horizontal = 1.0f;
-        }
-        else if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-
-        //Debug.Log("horizontal: "+horizontal +"vertical: " +vertical);
-
-        Vector2 position = transform.position;
-
-        position.x += 0.01f * horizontal;
-        position.y += 0.01f * vertical;
-
-        //Debug.Log("position x: " + position.x + "position y: " + position.y);
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2) transform.position + move * 0.01f;
         transform.position = position;
     }
 }
